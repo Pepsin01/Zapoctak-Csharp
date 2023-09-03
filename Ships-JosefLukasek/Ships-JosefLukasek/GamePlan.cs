8a4fd451b7994f5bf7c9989bcf245d346fdbacca
Josef Lukášek <pepalukasek@gmail.com>
2023-09-03 13:58:06 +0200

    Adding comments to game plan

--- Ships-JosefLukasek/Ships-JosefLukasek/GamePlan.cs
+++ Ships-JosefLukasek/Ships-JosefLukasek/GamePlan.cs
@@ -8,17 +8,31 @@ using System.Drawing;
 
 namespace Ships_JosefLukasek
 {
-
+    // Enumeration to represent the state of a square on the grid.
     enum SquareState { Free, Occupied, Hit }
+
+    // Enumeration to represent the state of the game plan.
     enum PlanState { Locked, Placing, Standby, Hidden }
 
+    // Represents a ship on the game plan grid.
     internal class Ship
     {
+        // Enumeration to specify the direction of the ship.
         public enum ShipDir { Down, Up, Left, Right }
+
+        // Current direction of the ship.
         public ShipDir dir { get; private set; }
+
+        // Length of the ship.
         public int Length { get; }
+
+        // Array to store the squares occupied by the ship.
         public Square[] occupiedSquares { get; private set; }
 
+        /// <summary>
+        /// Initializes a new ship with the specified length.
+        /// </summary>
+        /// <param name="length">The length of the ship.</param>
         public Ship(int length)
         {
             this.Length = length;
@@ -26,6 +40,11 @@ namespace Ships_JosefLukasek
             occupiedSquares = new Square[(Length * 3) + 2];
         }
 
+
+        /// <summary>
+        /// Rotates the ship 90 degrees clockwise.
+        /// </summary>
+        /// <returns>The new direction of the ship.</returns>
         public ShipDir Rotate()
         {
             switch (dir)
@@ -48,6 +67,9 @@ namespace Ships_JosefLukasek
             
         }
 
+        /// <summary>
+        /// Removes the ship from the grid.
+        /// </summary>
         public void Remove()
         {
             foreach (var s in occupiedSquares)
@@ -59,6 +81,8 @@ namespace Ships_JosefLukasek
             occupiedSquares = new Square[(Length * 3) + 2];
         }
     }
+
+    // Represents a square on the game plan grid.
     internal class Square
     {
         SquareState state = SquareState.Free;
@@ -93,25 +117,59 @@ namespace Ships_JosefLukasek
         public Button button { get; }
         public Ship? ship { get; set; }
         int occupationCounter = 0;
+
+        /// <summary>
+        /// Initializes a new square with the specified state and associated button.
+        /// </summary>
+        /// <param name="state">The initial state of the square.</param>
+        /// <param name="button">The button associated with the square.</param>
         public Square(SquareState state, Button button)
         {
             this.state = state;
             this.button = button;
         }
     }
+
+    // Represents the game plan grid and logic.
     internal class GamePlan
     {
+        // The form associated with the game plan.
         ShipsForm form;
-        Square[,] grid;
-        int squareSize = 40;
-        int defaultLeft;
-        int defaultTop;
-        int[] ships = new[]{ 4, 3, 2, 1 };
-        public PlanState state { get; set; }
-        Ship? currentShip;
-        Button lastlyHoveredOn;
-        Action<bool> shootCallBack;
 
+        // The grid of squares.
+        Square[,] grid;
+
+        // Size of a square in pixels.
+        const int squareSize = 40;
+
+        // Default left position of the grid.
+        int defaultLeft;
+
+        // Default top position of the grid.
+        int defaultTop;
+
+        // Predefined lengths of ships.
+        int[] ships = new[] { 4, 3, 2, 1 };
+
+        // Current state of the game plan.
+        public PlanState state { get; set; }
+
+        // The ship currently being placed.
+        Ship? currentShip;
+
+        // The last button the mouse was hovering on.
+        Button lastlyHoveredOn;
+
+        // The callback action for shooting on a square.
+        Action<bool> shootCallBack; 
+
+        /// <summary>
+        /// Initializes a new game plan with the specified form, default left and top positions, and shoot callback action.
+        /// </summary>
+        /// <param name="form">The form associated with the game plan.</param>
+        /// <param name="defaultLeft">The default left position of the grid.</param>
+        /// <param name="defaultTop">The default top position of the grid.</param>
+        /// <param name="shootCallBack">The callback action for shooting on a square.</param>
         public GamePlan(ShipsForm form, int defaultLeft, int defaultTop, Action<bool> shootCallBack)
         {
             this.form = form;
@@ -123,6 +181,10 @@ namespace Ships_JosefLukasek
             RefreshPlacingGraphics();
         }
 
+        /// <summary>
+        /// Creates the game plan grid with squares and buttons.
+        /// </summary>
+        /// <returns>The grid of squares.</returns>
         Square[,] CreatePlan()
         {
             Square[,] grid = new Square[10, 10];
@@ -150,6 +212,12 @@ namespace Ships_JosefLukasek
             return grid;
         }
 
+        /// <summary>
+        /// Creates neighbors for the specified square in the grid.
+        /// </summary>
+        /// <param name="i">The row index of the square.</param>
+        /// <param name="j">The column index of the square.</param>
+        /// <param name="grid">The game plan grid.</param>
         void MakeNeighborsForSquare(int i, int j, Square[,] grid)
         {
             if (IsExistingSquare(i + 1, j) && grid[i + 1, j] != null)
@@ -174,6 +242,11 @@ namespace Ships_JosefLukasek
             }
         }
 
+        /// <summary>
+        /// Converts a button to grid coordinates.
+        /// </summary>
+        /// <param name="btn">The button to convert.</param>
+        /// <returns>The grid coordinates as a tuple (i, j).</returns>
         public (int i, int j) BtnToCoordinates(Button btn)
         {
             int i = (btn.Left - defaultLeft) / squareSize;
@@ -181,6 +254,11 @@ namespace Ships_JosefLukasek
             return (i, j);
         }
 
+        /// <summary>
+        /// Handles the click event on a square in the game plan grid.
+        /// </summary>
+        /// <param name="sender">The sender object.</param>
+        /// <param name="e">The event arguments.</param>
         public void OnPlanClick(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
@@ -212,6 +290,11 @@ namespace Ships_JosefLukasek
 
         }
 
+        /// <summary>
+        /// Handles the hover event over a square in the game plan grid.
+        /// </summary>
+        /// <param name="sender">The sender object.</param>
+        /// <param name="e">The event arguments.</param>
         public void OnPlanHover(object? sender, EventArgs? e)
         {
             RefreshPlacingGraphics();
@@ -234,6 +317,11 @@ namespace Ships_JosefLukasek
             lastlyHoveredOn = btn;
         }
 
+        /// <summary>
+        /// Places the current ship on the game plan grid.
+        /// </summary>
+        /// <param name="pos">The position where the ship is being placed.</param>
+        /// <param name="buildMode">True if in build mode (placing the ship), false if in preview mode (hovering).</param>
         void PlaceCurrShip((int i, int j) pos, bool buildMode)
         {
 
@@ -311,6 +399,14 @@ namespace Ships_JosefLukasek
             }
         }
 
+        /// <summary>
+        /// Colors valid squares for ship placement.
+        /// </summary>
+        /// <param name="i">The row index.</param>
+        /// <param name="j">The column index.</param>
+        /// <param name="modi">The modifier for the row index based on ship direction.</param>
+        /// <param name="modj">The modifier for the column index based on ship direction.</param>
+        /// <param name="length">The length of the ship.</param>
         void ColorValidSquares(int i, int j, int modi, int modj, int length)
         {
             for (int x = 0; x < length; x++)
@@ -319,6 +415,14 @@ namespace Ships_JosefLukasek
             }
         }
 
+        /// <summary>
+        /// Colors invalid squares for ship placement.
+        /// </summary>
+        /// <param name="i">The row index.</param>
+        /// <param name="j">The column index.</param>
+        /// <param name="modi">The modifier for the row index based on ship direction.</param>
+        /// <param name="modj">The modifier for the column index based on ship direction.</param>
+        /// <param name="length">The length of the ship.</param>
         void ColorInvalidSquares(int i, int j, int modi, int modj, int length)
         {
             for (int x = 0; x < length; x++)
@@ -330,6 +434,14 @@ namespace Ships_JosefLukasek
             }
         }
 
+        /// <summary>
+        /// Occupies squares on the game plan grid with the current ship.
+        /// </summary>
+        /// <param name="i">The row index.</param>
+        /// <param name="j">The column index.</param>
+        /// <param name="modi">The modifier for the row index based on ship direction.</param>
+        /// <param name="modj">The modifier for the column index based on ship direction.</param>
+        /// <param name="length">The length of the ship.</param>
         void OccupieSquares(int i, int j, int modi, int modj, int length)
         {
             for (int x = 0; x < length; x++)
@@ -368,6 +480,15 @@ namespace Ships_JosefLukasek
             }
         }
 
+        /// <summary>
+        /// Checks if a given position is valid for ship placement.
+        /// </summary>
+        /// <param name="i">The row index.</param>
+        /// <param name="j">The column index.</param>
+        /// <param name="modi">The modifier for the row index based on ship direction.</param>
+        /// <param name="modj">The modifier for the column index based on ship direction.</param>
+        /// <param name="length">The length of the ship.</param>
+        /// <returns>True if the position is valid for ship placement, otherwise false.</returns>
         bool IsPositionValid(int i, int j, int modi, int modj, int length)
         {
             for (int x = 0; x < length; x++)
@@ -377,7 +498,13 @@ namespace Ships_JosefLukasek
             }
             return true;
         }
-        
+
+        /// <summary>
+        /// Checks if a square at the specified coordinates exists on the game plan grid.
+        /// </summary>
+        /// <param name="i">The row index.</param>
+        /// <param name="j">The column index.</param>
+        /// <returns>True if the square exists, otherwise false.</returns>
         bool IsExistingSquare(int i, int j)
         {
             if (i > 9 || j > 9 || i < 0 || j < 0)
@@ -385,6 +512,10 @@ namespace Ships_JosefLukasek
             return true;
         }
 
+        /// <summary>
+        /// Removes a ship from the game plan grid at the specified position.
+        /// </summary>
+        /// <param name="pos">The position of the ship to be removed.</param>
         void RemoveShip((int i, int j) pos)
         {
             Ship? ship = grid[pos.i, pos.j].ship;
@@ -396,6 +527,10 @@ namespace Ships_JosefLukasek
             }
         }
 
+        /// <summary>
+        /// Picks a ship of the specified length to be placed on the game plan grid.
+        /// </summary>
+        /// <param name="length">The length of the ship to pick.</param>
         public void PickShip(int length)
         {
             if (ships[length - 1] > 0 && currentShip == null)
@@ -412,12 +547,18 @@ namespace Ships_JosefLukasek
             }
         }
 
+        /// <summary>
+        /// Rotates the current ship 90 degrees clockwise.
+        /// </summary>
         public void RotateCurrShip()
         {
             currentShip?.Rotate();
             OnPlanHover(null, null);
         }
 
+        /// <summary>
+        /// Refreshes the graphics of the game plan grid during ship placement.
+        /// </summary>
         void RefreshPlacingGraphics()
         {
             foreach (var s in grid)
@@ -435,6 +576,9 @@ namespace Ships_JosefLukasek
             }
         }
 
+        /// <summary>
+        /// Refreshes the in-game graphics of the game plan grid.
+        /// </summary>
         void RefreshInGameGraphics()
         {
             if (PlanState.Hidden == state)
@@ -477,6 +621,10 @@ namespace Ships_JosefLukasek
 
         }
 
+        /// <summary>
+        /// Converts the game plan grid to a string representation.
+        /// </summary>
+        /// <returns>A string representing the game plan grid.</returns>
         public override string ToString()
         {
             StringBuilder result = new StringBuilder();
@@ -494,12 +642,23 @@ namespace Ships_JosefLukasek
             return result.Remove(result.Length - 1, 1).ToString();
         }
 
+        /// <summary>
+        /// Checks if a square at the specified position has been hit.
+        /// </summary>
+        /// <param name="pos">The position to check.</param>
+        /// <returns>True if the square has been hit, otherwise false.</returns>
         bool IsHit((int i, int j) pos)
         {
             if (grid[pos.i, pos.j].State == SquareState.Hit)
                 return true;
             return false;
         }
+
+        /// <summary>
+        /// Shoots at a square on the game plan grid.
+        /// </summary>
+        /// <param name="pos">The position to shoot at.</param>
+        /// <returns>True if a ship is hit, otherwise false.</returns>
         bool ShootOnSquare((int i, int j) pos)
         {
             grid[pos.i, pos.j].State = SquareState.Hit;
@@ -508,6 +667,9 @@ namespace Ships_JosefLukasek
             return false;
         }
 
+        /// <summary>
+        /// Resizes the game plan grid based on the size of the form's client area.
+        /// </summary>
         public void Resize()
         {
             int newLeft = (form.ClientRectangle.Width / 2) - (10 * squareSize);
