0c9caf0c47ef8a67c64668636c3ed1f8db7e5371
Josef Lukášek <pepalukasek@gmail.com>
2023-09-03 13:59:29 +0200

    Adding network handler

--- Ships-JosefLukasek/Ships-JosefLukasek/NetworkHandler.cs
+++ Ships-JosefLukasek/Ships-JosefLukasek/NetworkHandler.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Ships_JosefLukasek
 {
     internal class NetworkHandler
     {
+        public NetworkHandler(Action<string> callBack, bool server)
+        {
 
-    }
+        }
+
+        public void Send(string message)
+        {
+            
+        }
+    }    
 }
